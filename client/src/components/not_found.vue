35202be807ccea83491769c1d42ced6acb4520f1
RadomirTz <helo.vot2018@yandex.ru>
2021-04-05 18:29:28 +0300

    'gogo'

---
+++ client/src/components/not_found.vue
@@ -0,0 +1,8 @@
+<template>
+    <div style='box-shadow: 0 0 5px 2px #00404C;font-family: Noto Sans, sans-serif; padding-left: 30px; padding:20px;height:100%;border-radius:10px;background-color:rgba(0, 42, 50, 0.95); margin-top:90px; margin-left:100px; margin-right:100px;color: #BABABA;'>
+        <h1>Ошибка 404!</h1>
+        <p>Такой страницы не существует!</p>
+        <p>Возможно вы ошиблись адресом</p>
+        <a href='http://localhost:8080/#/'>Главная страница</a>
+    </div>
+</template>
